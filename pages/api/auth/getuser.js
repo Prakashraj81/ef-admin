b2f82dca339316e2c59dcc4e1819e81f8ceaacbf
Prakashraj81 <prakash.raj@g-japan.com>
2023-11-25 20:06:49 +0530

    correction changes-8

--- pages/api/auth/getuser.js
+++ pages/api/auth/getuser.js
@@ -1,10 +1,25 @@
 import { sql, config } from '/config';
+import jwt from 'jsonwebtoken';
+
+const secretKey = process.env.SENDINBLUE_API_KEY;
+
+function generateAuthToken(user) {
+    const payload = {
+        id: user.Id,
+        email: user.Email,  
+        pwd: user.Password,      
+    };
+    return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+}
+
 
 export default async (req, res) => {
 let { PhoneNo, Password } = req.body;
   try {
     await sql.connect(config);
     let result = await sql.query`Select * from Authentication where PhoneNo=${PhoneNo} and Password=${Password} and Active=1`;
+     // Generate and return a JWT token
+    const token = generateAuthToken(user);      
     // if (Id !== 0) {
     //   result = await sql.query`Select * from Authentication where Id=${Id}`;
     // } else {
@@ -13,7 +28,7 @@ let { PhoneNo, Password } = req.body;
 
     if (result.recordset.length !== 0) {
       const user = result.recordset;
-      res.status(200).json({ user });
+      res.status(200).json({ token, user });
     } else {
       res.status(401).json({ error: 'User list not found' });
     }
