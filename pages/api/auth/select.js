fc6a350de49b6c451c85f64dd773dfdb2452c17e
Prakashraj81 <prakash.raj@g-japan.com>
2023-11-25 20:38:50 +0530

    correction changes-9

--- pages/api/auth/select.js
+++ pages/api/auth/select.js
@@ -21,15 +21,15 @@ export default async (req, res) => {
         // if(result.recordset.length === 0) {
         //     result = await sql.query`Select Id from Authentication where Email=${UserName} and Password=${Password} and Active=1`;
         // }     
-        if (result.recordset.length !== 0) {           
-            const user = result.recordset[0];
-            // Generate and return a JWT token
-        //    const token = generateAuthToken(user);             
-            res.status(200).json({ user });                    
-        } else {
-            // Invalid credentials            
-            res.status(401).json({ error: 'Invalid credentials' });
-        }
+        // if (result.recordset.length !== 0) {           
+        //     const user = result.recordset[0];
+        //     // Generate and return a JWT token
+        // //    const token = generateAuthToken(user);             
+        //     res.status(200).json({ user });                    
+        // } else {
+        //     // Invalid credentials            
+        //     res.status(401).json({ error: 'Invalid credentials' });
+        // }
         res.json(result.recordset);
     } catch (error) {
         console.error('Error executing query:', error);
