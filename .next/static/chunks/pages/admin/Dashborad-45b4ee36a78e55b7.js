c0cd5259c69d1046d38949f412458fc8e935c22c
Prakashraj81 <prakash.raj@g-japan.com>
2024-02-10 23:41:13 +0530

    Customer enquiry form development

--- .next/static/chunks/pages/admin/Dashborad-45b4ee36a78e55b7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7044],{7632:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/Dashborad",function(){return u(8495)}])}},function(n){n.O(0,[5675,1664,1103,8218,5121,6886,4638,6057,6016,1629,3967,8495,9774,2888,179],function(){return n(n.s=7632)}),_N_E=n.O()}]);
