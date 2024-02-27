88a5ea55720c55aef173a7cdb6fc05f63fc9a0c9
Prakashraj81 <prakash.raj@g-japan.com>
2024-02-27 22:57:18 +0530

    Email format template design added

--- pages/api/auth/auth-otp-verification-email.js
+++ pages/api/auth/auth-otp-verification-email.js
@@ -34,7 +34,129 @@ export default async (req, res) => {
             from: 'kassaposprakash@gmail.com',
             to: 'aprakashraj81@gmail.com',
             subject: 'Email OTP Verification',
-            text: `Your OTP for email verification is: ${otp}`
+            html: `<!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <title>Eftapei Mind Makeover</title>
+            
+                <link
+                  href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+                  rel="stylesheet"
+                />
+              </head>
+              <body
+                style="
+                  margin: 0;
+                  font-family: 'Poppins', sans-serif;
+                  background: #ffffff;
+                  font-size: 14px;
+                "
+              >
+                <div
+                  style="
+                    max-width: 680px;
+                    margin: 0 auto;
+                    padding: 45px 30px 60px;
+                    background: #f4f7ff;
+                    background-image: url(https://ef-admin-eight.vercel.app/about/bg-about.svg);
+                    background-repeat: no-repeat;
+                    background-size: 800px 452px;
+                    background-position: top center;
+                    font-size: 14px;
+                    color: #434343;
+                  "
+                >
+                  <header>
+                    <table style="width: 100%;">
+                      <tbody>
+                        <tr style="height: 0;">
+                          <td>
+                            <img
+                              alt=""
+                              src="https://ef-admin-eight.vercel.app/logo/header-logo.jpg"
+                              height="30px"
+                            />
+                          </td>
+                          <td style="text-align: right;">
+                            <span
+                              style="font-size: 16px; line-height: 30px; color: #ffffff;"
+                              >${CurrentDate}</span
+                            >
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </header>
+            
+                  <main>
+                    <div
+                      style="
+                        margin: 0;
+                        margin-top: 70px;
+                        padding: 92px 30px 115px;
+                        background: #ffffff;
+                        border-radius: 30px;
+                        text-align: center;
+                      "
+                    >
+                      <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+                        <h1
+                          style="
+                            margin: 0;
+                            font-size: 24px;
+                            font-weight: 500;
+                            color: #1f1f1f;
+                          "
+                        >
+                          Your OTP
+                        </h1>
+                        <p
+                          style="
+                            margin: 0;
+                            margin-top: 17px;
+                            font-size: 16px;
+                            font-weight: 500;
+                          "
+                        >
+                          Hey Eftapei admin or emaployee,
+                        </p>
+                        <p
+                          style="
+                            margin: 0;
+                            margin-top: 17px;
+                            font-weight: 500;
+                            letter-spacing: 0.56px;
+                          "
+                        >
+                          Thank you for choosing Eftapei Mind makeover. Use the following OTP
+                          to complete the procedure to change your email address. OTP is
+                          valid for
+                          <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
+                          Do not share this code with others, including Eftapei
+                          employees.
+                        </p>
+                        <p
+                          style="
+                            margin: 0;
+                            margin-top: 60px;
+                            font-size: 40px;
+                            font-weight: 600;
+                            letter-spacing: 25px;
+                            color: #ba3d4f;
+                          "
+                        >
+                          ${otp}
+                        </p>
+                      </div>
+                    </div>                            
+                  </main>                
+                </div>
+              </body>
+            </html>
+            `
         };
         await transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
--- pages/api/auth/authotpselect.js
+++ pages/api/auth/authotpselect.js
@@ -7,6 +7,21 @@ export default async (req, res) => {
     let result;
     if (EnterOTP !== "") {
       result = await sql.query`Select auth_otp from auth_otp_table where auth_otp=${EnterOTP}`;
+      let result_1 = await sql.query`DECLARE @InputOTP NVARCHAR(10) = ${EnterOTP};
+      DECLARE @ExpirationMinutes INT = 3;
+      
+      IF EXISTS (
+          SELECT auth_otp FROM auth_otp_table WHERE auth_otp = @InputOTP AND created_date >= DATEADD(MINUTE, -@ExpirationMinutes, GETDATE())
+      )
+      BEGIN
+          -- The OTP is valid
+          PRINT 'OTP is valid';
+      END
+      ELSE
+      BEGIN
+          -- The OTP has expired
+          PRINT 'OTP has expired';
+      END;`;
     } else {
       result = await sql.query`Select * from auth_otp_table`;
     }
